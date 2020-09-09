a24d7cc8ae9e60803cc79873c9840c527e5e0702
mgechev <mgechev@gmail.com>
2020-09-10 00:48:43 +0300

    Remove the routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DragDropModule } from '@angular/cdk/drag-drop';
@@ -22,7 +21,6 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
   declarations: [AppComponent, TaskDialogComponent, TaskComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     DragDropModule,
     MatCardModule,
