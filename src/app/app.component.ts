0f938e9253909f96039b9fd58c026c021463301b
mgechev <mgechev@gmail.com>
2020-09-12 00:02:02 +0300

    Add delete and edit functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,7 +37,24 @@ export class AppComponent {
       .subscribe((result: TaskDialogResult) => this.todo.push(result.task));
   }
 
-  editTask(list: string, task: Task): void {}
+  editTask(list: 'done' | 'todo' | 'inProgress', task: Task): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task,
+        enableDelete: true,
+      },
+    });
+    dialogRef.afterClosed().subscribe((result: TaskDialogResult) => {
+      const dataList = this[list];
+      const taskIndex = dataList.indexOf(task);
+      if (result.delete) {
+        dataList.splice(taskIndex, 1);
+      } else {
+        dataList[taskIndex] = task;
+      }
+    });
+  }
 
   drop(event: CdkDragDrop<Task[]>): void {
     if (event.previousContainer === event.container) {
