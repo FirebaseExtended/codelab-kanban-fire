23dd4fc86f0a8b4e4d00037ca716377a5165fa5f
mgechev <mgechev@gmail.com>
2020-09-11 20:23:45 +0300

    Add drag & drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Task } from './task/task';
+import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -17,4 +18,20 @@ export class AppComponent {
       description: 'Using Firebase and Angular create a Kanban app!',
     },
   ];
+  inProgress: Task[] = [];
+  done: Task[] = [];
+
+  editTask(list: string, task: Task): void {}
+
+  drop(event: CdkDragDrop<Task[]>): void {
+    if (event.previousContainer === event.container) {
+      return;
+    }
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { TaskComponent } from './task/task.component';
 import { MatCardModule } from '@angular/material/card';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [AppComponent, TaskComponent],
-  imports: [BrowserModule, MatToolbarModule, MatIconModule, MatCardModule],
+  imports: [BrowserModule, MatToolbarModule, MatIconModule, MatCardModule, DragDropModule],
   providers: [],
   bootstrap: [AppComponent],
 })
